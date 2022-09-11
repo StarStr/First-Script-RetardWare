cb77a06f1e21dbae7f2bd9d6c919baba451b0913
StarStr <113265319+StarStr@users.noreply.github.com>
2022-09-10 21:26:39 -0500

    Add files via upload

---
+++ Test Roblox.lua
@@ -0,0 +1,47 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("RetardWare", "Sentinel")
+
+
+--tabs
+
+--Main
+  local Main = Window:NewTab("Main")
+  local MainSection = Main:NewSection("Main")
+
+  MainSection:NewButton("Back/Front flip", "Makes you Flip", function()
+    loadstring(game:HttpGet('https://pastebin.com/raw/7wDcPtLk'))()
+end)
+
+MainSection:NewToggle("Super-Human", "Go fast, jump high", function(state)
+    if state then
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 120
+        game.Players.LocalPlayer.Character.Humanoid.jumpPower = 120  --Come Back To fix {
+    else 
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+        game.Players.LocalPlayer.Character.Humanoid.jumpPower = 50   --Come Back to Fix }
+    end
+end)
+
+MainSection:NewButton("Infinite Yield", "Infinite Yield", function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/Infiniteyield/master/source'),true))()
+end)
+
+--LOCAL PLAYER
+local Player = Window:NewTab("player")
+local PlayerSection = Player:NewSection("Player")
+
+PlayerSection:NewSlider("WalkSpeed", "Speed", 500, 16, function(s) -- 500 (MaxValue) | 16 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+PlayerSection:NewSlider("JumpPower", "Jump", 500, 50, function(s) -- 500 (MaxValue) | 50 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+
+--other
+local Other = Window:NewTab("Other")
+local OtherSection = Other:NewSection("Other")
+
+OtherSection:NewButton("Chat Spoofer", "lets you chat for other people" , function()
+ loadstring(game:HttpGet(('https://pastebin.com/raw/djBfk8Li'),true))()
+end)
